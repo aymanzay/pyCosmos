321f43d6ba41bc533b61c90e51fc05eeff20f842
Ayman Zeine <a.zeine.96@gmail.com>
2019-05-06 17:32:50 -0700

    add code to convert analysis df to nxm numpy array for clustering purposes added clustering functionality for analysis data

--- dfops.py
+++ dfops.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from pyspark.sql.functions import udf
 from pyspark.mllib.linalg import Vectors, VectorUDT
+from pyspark.ml.feature import VectorAssembler
 
 def Merge(dict1, dict2):
     res = {**dict1, **dict2}
@@ -21,20 +23,22 @@ def refactorAnalysisDF(lib_analysis, ids, sql_context):
             if isinstance(metadata, (list,)):
                 arrays = []
                 for item in metadata: #iterate list of dicts
-                    subvals = list(item.values())
+                    subvals = np.asarray(list(item.values()))
                     arrays.append(subvals)
                 temp_d = arrays #now key -> array, instead of key to array[dicts]
-                flatten = [iteml for sublist in temp_d for iteml in sublist]
-                factored.append(flatten)
-            else:
-                items = list(metadata.values())
-                temp_d = items
                 #flatten = [iteml for sublist in temp_d for iteml in sublist]
                 factored.append(temp_d)
+            else:
+                items = np.asarray(list(metadata.values()))
+                #temp_d = items
+                #flatten = [iteml for sublist in temp_d for iteml in sublist]
+                #factored.append(temp_d)
         full_arr.append(factored)
 
-    analysis_df = pd.DataFrame(full_arr, columns=keylist)
-    analysis_df = analysis_df.drop(columns=['meta'])
+    # Spark data frame vectorization closure; not applicable for data structure or sklearn processing
+    '''
+    #analysis_df = pd.DataFrame(full_arr, columns=keylist)
+    #analysis_df = analysis_df.drop(columns=['meta'])
 
     s_analysis = sql_context.createDataFrame(analysis_df)
     list_to_vector_udf = udf(lambda l: Vectors.dense(l), VectorUDT())
@@ -46,4 +50,32 @@ def refactorAnalysisDF(lib_analysis, ids, sql_context):
         list_to_vector_udf(s_analysis["segments"]).alias("segments")
     )
 
-    return df_w_vectors
+    #assembler = VectorAssembler(inputCols=df_w_vectors.schema.names, outputCol="features")
+    #v_analysis = assembler.transform(df_w_vectors)
+    '''
+    full_arr = np.asarray(full_arr)
+    largest = 0
+    for i in range(full_arr.shape[0]):
+        for index in range(0, 4):
+            length = np.asarray(full_arr[i][index]).shape[0]
+            if length > largest:
+                largest = length
+
+    ml_analysis = np.zeros((full_arr.shape[0], largest))
+    for i in range(ml_analysis.shape[0]):
+        temp_a = []
+        for index in range(0, 4):
+            temp = np.asarray(full_arr[i][index])
+            temp_a.append(temp)
+        full_a = []
+        ##OPTIMIZE
+        for x in temp_a:
+            x = np.asarray(x).flatten()
+            for y in x:
+                full_a.append(y)
+        full_a = np.asarray(full_a)
+
+        for zi, z in zip(range(len(full_a)), range(len(ml_analysis[i]))):
+            ml_analysis[i][z] = full_a[zi]
+
+    return np.asarray(ml_analysis)
--- scheme.py
+++ scheme.py
@@ -1,22 +1,34 @@
+print(__doc__)
+
 from dag import *
 from spotapi import *
 from dfops import *
 import spotipy
+
+import time
 import warnings
 import pickle
 import numpy as np
 import pandas as pd
+
+from spark_sklearn import Converter
 from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.sql import Row
 import pyspark.sql.types as pst
-from pyspark.ml.clustering import KMeans
-from pyspark.ml.evaluation import ClusteringEvaluator
+#from pyspark.ml.clustering import KMeans
+#from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.ml.feature import VectorAssembler
+
 from sklearn.decomposition import PCA
-#from sklearn.cluster import KMeans, MeanShift
+from sklearn.cluster import KMeans, MeanShift
 from sklearn.model_selection import train_test_split 
 from sklearn.preprocessing import StandardScaler  
-from sklearn.neighbors import KNeighborsClassifier  
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets.samples_generator import make_blobs
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -53,25 +65,57 @@ if __name__ == '__main__':
     ids = library['id']
 
     #create analysis dataframe
-    v_analysis = refactorAnalysisDF(lib_analysis, ids, sql_sc)
+    print("Extracting analysis data")
+    start = time.time()
+    m_analysis = refactorAnalysisDF(lib_analysis, ids, sql_sc)
+    end = time.time()
+    print('Extracted in', (end-start), 'seconds')
+    #possible approach may be to process each column separately
 
     #lib_features = library.drop(columns=['analysis'])
     s_features = s_main
+    print("Extracting feature data")
     assembler = VectorAssembler(inputCols=s_features.schema.names, outputCol="features")
     v_features = assembler.transform(s_features)
 
     #lib_tech = library.as_matrix(columns=['tempo', 'key', 'loudness', 'valence', 'time_signature', 'liveness', 'energy', 'danceability'])
     s_tech = s_main.select('tempo', 'key', 'loudness', 'valence', 'time_signature', 'liveness', 'energy', 'danceability')
+    print("Extracting tech data")
     assembler = VectorAssembler(inputCols=s_tech.schema.names, outputCol="tech_features")
     v_tech = assembler.transform(s_tech)
 
     #lib_song_info = library.as_matrix(columns=['artist', 'genre', 'speechiness', 'acousticness', 'instrumentalness'])
     s_info = s_main.select('speechiness', 'acousticness', 'instrumentalness')
+    print("Extracting song info data")
     assembler = VectorAssembler(inputCols=s_info.schema.names, outputCol="tech_features")
     v_info = assembler.transform(s_info)
 
+    print('Converting ')
+    start = time.time()
+    converter = Converter(sc)
+    features = converter.toPandas(v_features)
+    m_features = np.asarray(features.values)
+    end = time.time()
+    print('Converted in', (end-start), 'seconds')
+
+    print("Performing analysis clustering")
     #get initial root vectors
     #init meanshift clustering model
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    X, _ = make_blobs(n_samples=m_analysis.shape[0], centers=centers, cluster_std=0.6)
+
+    start = time.time()
+    clustering = MeanShift(n_jobs=-1)
+    clustering.fit(m_analysis)
+
+    labels = clustering.labels_
+    cluster_centers = clustering.cluster_centers_
+    labels_unique = np.unique(labels)
+    n_clusters = len(labels_unique)
+    end = time.time()
+    print('Clustered in', ((end - start)/60), 'minutes')
+
+    print(clustering.cluster_centers_)
 
     kmeans = KMeans().setK(12).setSeed(1)
     model = kmeans.fit(v_features)
@@ -90,8 +134,6 @@ if __name__ == '__main__':
     
     #get list of centroids to extract root vectors
 
-    #centroids = labels.cluster_centers_ # array, [n_clusters, n_features]
-    #print(centroids)
     #get vertex distances in order to assign them to weights
     #distances = []
     #for i, (cx, cy) in enumerate(centroids):
